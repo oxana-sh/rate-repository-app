143f3f927b6be7fd8f09197b912fb12261ed3b3b
oxana-sh <121@jazz.su>
2023-11-12 07:27:55 +0300

    ew ppp

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Привет! Это я твоя тень)</Text>
+      <Text>Привет! Это я твоя тень :)</Text>
       <StatusBar style="auto" />
     </View>
   );
